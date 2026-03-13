eb641b419e31c77ba50beec4be323a733d0ca059
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-03-12 22:03:07 -0500

    make changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,9 +16,9 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>220 😇</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Nathan is a <code>Chud</code> and so is <code>Abraham</code>
           </p>
         </div>
         <button
