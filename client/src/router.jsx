b07811886287fe08f8ae19a32d89f4745c2cb65f
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-03-25 20:55:42 -0500

    create router with placeholders

---
+++ client/src/router.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from 'react-router-dom';
+import { App } from './App';
+/* Import all pages here */
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <NotFound /> TODO: do this later
+    children: [
+      {
+        index: true,
+        element: <p>Home page here</p>,
+      },
+      {
+        path: 'about',
+        element: <p>About page here</p>
+      },
+      {
+        path: 'Our Team',
+        element: <p>Our Team page here</p>
+      },
+      {
+        path: 'Events',
+        element: <p>Events page here</p>
+      },
+      {
+        path: 'Contact',
+        element: <p>Contact page here</p>
+      },
+    ],
+  },
+]);
