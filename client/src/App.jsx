06935deb98d9650be9588de0c576f7f0dd964cb2
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-03-28 16:08:38 -0500

    add Navbar and basic structure of website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Navbar from './components/navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <div className='bg-white min-h-screen'>
+      <Navbar />
+      <main>
+      <Outlet />
+      </main>
+      {/* <Footer />*/}
+    </div>
   )
 }
 
