c24a5bf9d23ec38f53f78f915536a0cdc3cecb3d
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-03-30 21:10:47 -0500

    show text over video

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import groupPictureUrl from '/220GroupPictureMarch.png';
 import HeroVideo from './HeroVideo';
 
 function Hero() {
   return (
-    <section className='relative w-full h-screen min-h-80 max-h-170 overflow-hidden'>
+    <section className='relative w-full h-screen min-h-80 max-h-190 overflow-hidden'>
       <HeroVideo />
 
       <div className='absolute inset-0 bg-black/40' />
 
-      <div className='relative z-10 h-full flex-col items-center'>3
-        <div className='container-custom text-white'>
+      <div className='absolute inset-0 z-20 flex items-center'>
+        <div className='px-4 sm:px-6 lg:px-10 text-white max-w-3xl'>
           <h1 className='text-4xl md:text-6xl font-bold'>Welcome to 220 Youth</h1>
-          <p className='mt-4 max-w-xl text-base md:text-lg'>
+          <p className='mt-4 text-base md:text-lg'>
             Growing together in faith, community, and purpose.
           </p>
         </div>
