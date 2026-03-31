20c5b5456983d031d03a31941d496f694b814d5b
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-03-30 20:37:05 -0500

    implement HeroVideo into Hero component

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import groupPictureUrl from '/220GroupPictureMarch.png';
+import HeroVideo from './HeroVideo';
 
 function Hero() {
   return (
     <section className='relative w-full h-screen min-h-80 max-h-170 overflow-hidden'>
-      <img
-        src={groupPictureUrl}
-        alt='220 Youth Group'
-        className='absolute inset-0 w-full h-full object-cover object-center'
-      />
+      <HeroVideo />
 
       <div className='absolute inset-0 bg-black/40' />
 
-      <div className='relative z-10 h-full flex items-center'>
+      <div className='relative z-10 h-full flex-col items-center'>3
         <div className='container-custom text-white'>
           <h1 className='text-4xl md:text-6xl font-bold'>Welcome to 220 Youth</h1>
           <p className='mt-4 max-w-xl text-base md:text-lg'>
