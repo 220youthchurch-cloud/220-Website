b4e9288520116e8dea20a9b2f96e7fc2143365ff
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-04-02 16:59:42 -0500

    create and add Motto component to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './../components/home/Hero'
 import WelcomeTo220 from '../components/home/WelcomeTo220'
+import Motto from '../components/home/Motto'
 import Servicetime from '../components/home/Servicetime'
 const Home = () => {
   return (
     <>
       <Hero />
       <WelcomeTo220 />
+      <Motto />
     </>
   )
 }
