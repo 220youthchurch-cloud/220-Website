0887021f8c8611176102ec2743610e96b8befba8
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-04-03 18:28:46 -0500

    update selection color for webpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
-    <div className='bg-white min-h-screen'>
+    <div className='bg-white min-h-screen selection:bg-[#97afbe]'>
       <Navbar />
       <main>
       <Outlet />
       </main>
-      {/* <Footer />*/}
+      <Footer />
     </div>
   )
 }
