24ab8f88b7cef1bb21a067b8f769251cdbbf9c05
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-04-04 17:18:35 -0500

    add socials data for YouTube, TikTok, and Instagram platforms

---
+++ client/src/data/socials.js
@@ -0,0 +1,14 @@
+export const socials = [
+  {
+    platform: "youtube",
+    link: "https://www.youtube.com/@220ForLife",
+  },
+  {
+    platform: "tiktok",
+    link: "https://www.tiktok.com/@220forlife_",
+  },
+  {
+    platform: "instagram",
+    link: "https://www.instagram.com/220forlife_/"
+  },  
+];
