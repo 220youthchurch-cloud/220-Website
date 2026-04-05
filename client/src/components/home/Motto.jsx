be147ffc5b0988d0d13f7d5bcccd1670649d1d3a
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-04-05 09:36:49 -0500

    add section padding and top margin to Motto component

--- client/src/components/home/Motto.jsx
+++ client/src/components/home/Motto.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Motto = () => {
   return (
-    <section className='bg-[#2c6598]'>
-      <div className='flex flex-col items-center text-center secondary-text gap-5'>
+    <section className='section-padding pt-1 bg-[#2c6598]'>
+      <div className='flex flex-col items-center text-center secondary-text gap-5 px-15'>
         <h1 className='header-font text-[50px] font-extrabold'>FOLLOWING JESUS. FINDING COMMUNITY.</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
