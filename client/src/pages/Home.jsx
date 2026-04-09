74bebc32bec1595abb4e493b57f66d8da40a768d
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-04-09 15:29:30 -0500

    add Servicetime and MinistryHighlights components to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Hero from './../components/home/Hero'
+import Servicetime from '../components/home/Servicetime'
+import MinistryHighlights from '../components/home/MinistryHighlights'
 import WelcomeTo220 from '../components/home/WelcomeTo220'
 import Motto from '../components/home/Motto'
 import LatestYouTubeVideo from '../components/home/LatestYouTubeVideo'
 import FollowUs from '../components/home/FollowUs'
-import Servicetime from '../components/home/Servicetime'
 const Home = () => {
   return (
     <>
       <Hero />
+      <Servicetime />
+      <MinistryHighlights />
       <WelcomeTo220 />
       <Motto />
       <LatestYouTubeVideo />
