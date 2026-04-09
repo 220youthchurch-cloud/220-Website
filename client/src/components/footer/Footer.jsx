865e1348dcd8ab93bf7d0b4613b457a120f79a40
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-04-09 16:42:23 -0500

    update Footer component to center align social media platforms

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
         {/* Header */}
         <div className='flex flex-col sm:flex-row sm:items-center sm:justify-between w-full gap-5 mb-10'>
           <Logo />
-          <div className='flex flex-col justify-center text-center'>
+          <div className='flex flex-col justify-center items-center'>
             <p className='body-font text-[#f7f7f7] text-base leading-7'>Make Disciples<br/>Live the Mission</p>
             <SocialMediaPlatforms size="sm" className='gap-3' displayTitle={false} />
           </div>
