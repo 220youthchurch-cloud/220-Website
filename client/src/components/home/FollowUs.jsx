962a851717c08ec24b0f0b27fee911113a3a94f7
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-04-08 20:45:30 -0500

    create and use FollowUs component

---
+++ client/src/components/home/FollowUs.jsx
@@ -0,0 +1,21 @@
+import { React } from 'react'
+
+import SocialMediaPlatforms from '../SocialMediaPlatforms'  
+
+const FollowUs = () => {
+  return (
+    <section className='section-padding text-center text-[#f7f7f7] bg-[#1a1a1a]'>
+      <div className='flex flex-col gap-7 items-center'>
+        <div>
+          <h2 className='header-font text-[4rem]'>Follow Us</h2>
+          <p className='body-font'>Follow us on social media to stay updated.</p>
+        </div>        
+        <div className='w-fit'>
+          <SocialMediaPlatforms iconSize={50} className='text-white justify-around text-3xl gap-10' />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default FollowUs
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Hero from './../components/home/Hero'
 import WelcomeTo220 from '../components/home/WelcomeTo220'
 import Motto from '../components/home/Motto'
-import LatestYouTubeVideo from '../components/home/LatestYouTubeVideo';
+import LatestYouTubeVideo from '../components/home/LatestYouTubeVideo'
+import FollowUs from '../components/home/FollowUs'
 import Servicetime from '../components/home/Servicetime'
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <WelcomeTo220 />
       <Motto />
       <LatestYouTubeVideo />
+      <FollowUs />
     </>
   )
 }
