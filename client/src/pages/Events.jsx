7b9f079ea99377320cfb99c747e0e0b559839528
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-04-16 20:30:15 -0500

    add Events page and update router to include it in navigation

---
+++ client/src/pages/Events.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import PageHeader from '../components/common/PageHeader'
+
+const Events = () => {
+  return (
+    <>
+      <PageHeader
+        title='Events'
+        description='Check out our upcoming events and join us for worship, fellowship, and service opportunities.'
+      />  
+    </>
+  )
+}
+
+export default Events
