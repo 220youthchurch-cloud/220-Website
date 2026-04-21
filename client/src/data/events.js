c567fa936ba5e75f01b3e41716e2f2e17a0487de
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-04-21 18:58:05 -0500

    add event flyers and details for Spring Break Conference, Basketball Tournament, and Consecrated Conference

--- client/src/data/events.js
+++ client/src/data/events.js
@@ -36,11 +36,53 @@ export const events = [
     startTime: "Feb 17 1:00 PM",
     endTime: "Feb 18 10:00 AM",
     location: "220 Church Sanctuary",
-    category: "Youth",
+    category: "Conference",
     recurring: false,
     isVideo: true,
     mediaPath: "/events/girls_conference/girls_conference.mp4"
   },
+  {
+    id: 4,
+    title: "Spring Break Conference",
+    description: "📣 Spring Break is almost here!! Join our conference “ARISE” March 14-16, and don’t forget to bring a friend! You don’t want to miss this!! Service will be in the Chapel (the adult service room) and on Sunday it will be in the 220 room.",
+    detailedDescription: "<h1>Spring Break Conference</h1><p>📣 Spring Break is almost here!! Join our conference “ARISE” March 14-16, and don’t forget to bring a friend! You don’t want to miss this!! Service will be in the Chapel (the adult service room) and on Sunday it will be in the 220 room.</p>",
+    date: "March 14-16, 2025",
+    startTime: "Mar 14 7:00 PM",
+    endTime: "Mar 16 10:00 AM",
+    location: "220 Church Sanctuary",
+    category: "Conference",
+    recurring: false,
+    isVideo: false,
+    mediaPath: "/events/spring_conference_2025/flyer.jpg"
+  },
+  {
+    id: 5,
+    title: "Basketball Tournament",
+    description: "PHASE ONE OF SATURDAY’S ACTIVITIES IS OUT. BE SURE TO FILL OUT THE FORM. WE GOT SO MUCH PLANNED! WE CANT WAIT TO SEE ALL OF YALL HERE. DON’T FORGET TO INVITE YOUR FRIENDS!!!!",
+    detailedDescription: "<h1>Basketball Tournament</h1><p>PHASE ONE OF SATURDAY’S ACTIVITIES IS OUT. BE SURE TO FILL OUT THE FORM. WE GOT SO MUCH PLANNED! WE CANT WAIT TO SEE ALL OF YALL HERE. DON’T FORGET TO INVITE YOUR FRIENDS!!!!</p>",
+    date: "March 15, 2025",
+    startTime: "Mar 15 4:00 PM",
+    endTime: "Mar 15 8:00 PM",
+    location: "11110 Garland Rd, Dallas, TX 75218",
+    category: "Recreation",
+    recurring: false,
+    isVideo: false,
+    mediaPath: "/events/basketball_tournament/flyer.jpg"
+  },
+  {
+    id: 6,
+    title: "Consecrated Conference",
+    description: "Join 220’s Young Adult Thanksgiving Conference……Consecrated!!!!! Experience three powerful days of worship, growth, and transformation! Be inspired by dynamic speakers: Pastor Nebiye Kelile, Ermmias Teclemariam, and Pastor Issrome Abraham.",
+    detailedDescription: "<h1>Consecrated Conference</h1><p>Join 220’s Young Adult Thanksgiving Conference……Consecrated!!!!! Experience three powerful days of worship, growth, and transformation! Be inspired by dynamic speakers: Pastor Nebiye Kelile, Ermmias Teclemariam, and Pastor Issrome Abraham.</p>",
+    date: "November 20-22, 2025",
+    startTime: "Nov 20 7:00 PM",
+    endTime: "Nov 22 10:00 AM",
+    location: "220 Church Sanctuary",
+    category: "Conference",
+    recurring: false,
+    isVideo: false,
+    mediaPath: "/events/consecrated_2025/flyer.jpg"
+  }
 ]
 
 const weekdayMap = {
