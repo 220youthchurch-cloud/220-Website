ccaeeccaf84c9c2c1102e9e76e76e5d9743cc69f
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-04-24 17:47:03 -0500

    add team pages for Discipleship, Hospitality, Outreach, Prayer, Tech, and Worship with headers

---
+++ client/src/pages/HospitalityTeam.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import PageHeader from '../components/common/PageHeader'
+import FadeIn from '../components/common/FadeIn'
+
+const HospitalityTeam = () => {
+  return (
+    <>
+      <PageHeader
+        title='Hospitality Team'
+        description='Discover the ministry that welcomes people and helps everyone feel at home.'
+      />
+    </>
+  )
+}
+
+export default HospitalityTeam
---
+++ client/src/pages/PrayerTeam.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import PageHeader from '../components/common/PageHeader'
+import FadeIn from '../components/common/FadeIn'
+
+const PrayerTeam = () => {
+  return (
+    <>
+      <PageHeader
+        title='Prayer Team'
+        description='Learn about the team devoted to intercession, prayer support, and spiritual care.'
+      />
+    </>
+  )
+}
+
+export default PrayerTeam
