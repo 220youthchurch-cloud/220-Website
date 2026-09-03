38497227ed990c1ae8522bf08bb4f6c00b7c2dc1
Saol Tesfaghebriel <saoltesf@gmail.com>
2026-09-02 19:32:30 -0500

    add payment methods data for Paypal, Zelle, and CashApp

---
+++ client/src/data/paymentMethods.js
@@ -0,0 +1,14 @@
+export const paymentMethods = [
+  {
+    method: "Paypal",
+    link: "https://www.paypal.com/donate?hosted_button_id=3Z7X5K2Y6V9J8",
+  },
+  {
+    method: "Zelle",
+    link: "https://www.zellepay.com/",
+  },
+  {
+    method: "CashApp",
+    link: "https://cash.app/$220ForLife",
+  },
+];
